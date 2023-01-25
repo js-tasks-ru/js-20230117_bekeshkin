2667aaba9679a7a4da2e0dab0f753af29a6700c2
iwanttomars <nikolay.bekeshkin@gmail.com>
2023-01-25 06:14:18 +0300

    3. Объекты и массивы, основы тестирования

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,34 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function (obj) {
+    if (path === 'undefined') {
+      return obj;
+    }
+    const props = path.split('.');
+    return getValue(obj, props);
+  };
+}
 
+/**
+ * Рекурсия для получения свойств с учетом вложенности
+ * @param obj - объект
+ * @param props - массив свойств, которые необходимо перебрать
+ *              '[name1, name2, name3, ..., nameN]',где 'nameN' - конечное свойство,
+ * @returns {*|undefined}
+ */
+function getValue(obj, props) {
+  // если объект неопределен, возвращаем его
+  if (obj === undefined) {
+    return obj;
+  }
+  // если конечное свойство
+  if (props.length === 1) {
+    // то возвращаем значение конечное свойства
+    return obj[props[0]];
+  } else {
+    // иначе ищем дальше в объекте, который располагается в конечном свойстве
+    const [prop, ...other] = props;
+    return getValue(obj[prop], other);
+  }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (string === undefined || size === undefined) {
+    return string;
+  }
+  let index = 0; //индекс для перебора строки
+  let count = 1; //счетчик повторов
+  let result = ''; //результирующая строка
+  while (size > 0 && index < string.length) {
+    // если результирующая строка содержит символы, и последний символ совпадает с текущим символом исходной строки
+    if (result.length > 0 && result[result.length - 1] === string[index]) {
+      // проверим счетчик повторов
+      if (count === size) {
+        // если он достиг максимум количества повторов, то перейдем к следующему символу в исходной строке,
+        // тем самым пропустим добавление в результрирующу строку
+        index++;
+        continue;
+      } else {
+        // увеличим счетчик, если не достигли максимум количества повторов
+        count++;
+      }
+    } else {
+      // сбросим счетчик в случае если символы различаются
+      count = 1;
+    }
+    // если дошли до сюда, добавим текущий символ к результирующей строке
+    result += string[index++];
+  }
+  return result;
 }
