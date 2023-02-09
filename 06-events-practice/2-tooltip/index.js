10a19ddc966bc7b4e7a4c0f67ccb5fcb6e065ede
iwanttomars <nikolay.bekeshkin@gmail.com>
2023-02-09 21:32:17 +0300

    5. События в браузере - практика

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,59 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  element;
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  pointerOver = (event) => {
+    const overDiv = event.target.closest('[data-tooltip]');
+    if (overDiv) {
+      this.render(overDiv.dataset.tooltip);
+      document.addEventListener('pointermove', this.pointerMove);
+    }
+  };
+
+  pointerMove = (event) => {
+    const shift = 10;
+    this.element.style.left = `${event.clientX + shift}px`;
+    this.element.style.top = `${event.clientY + shift}px`;
+  };
+
+
+  pointerOut = (event) => {
+    this.remove();
+    document.removeEventListener('pointermove', this.pointerMove);
+  };
+
+  initialize() {
+    document.addEventListener('pointerover', this.pointerOver);
+    document.addEventListener('pointerout', this.pointerOut);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.pointerOver);
+    document.removeEventListener('pointerout', this.pointerOut);
+    this.remove();
+  }
+
+  render(text) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML += text;
+
+    document.body.append(this.element);
   }
 }
 
